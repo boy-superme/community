0a86201e4c859f0492b59e0e08b59f67fdce4584
jiaohailong <2324557994@qq.com>
2019-12-05 19:28:29 +0800

    init repo

---
+++ src/main/java/lift/majiang/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package lift.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author 焦海龙
+ * @date spring的控制类 13:59 2019/12/4
+ */
+@Controller
+
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
